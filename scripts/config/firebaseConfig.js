9a2ebf6f97e5098141bb65baa870299bc30130f1
Ming-beginner <lengocminh310808@gmail.com>
2021-12-31 20:14:00 +0700

    new

--- scripts/config/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyD_csb12NMusiY36GiELVTjCJm1XKp4nBs",
-  authDomain: "authentication-practice-c5a1f.firebaseapp.com",
-  projectId: "authentication-practice-c5a1f",
-  storageBucket: "authentication-practice-c5a1f.appspot.com",
-  messagingSenderId: "276023611911",
-  appId: "1:276023611911:web:20da52981c3dc588b9d0a2",
-  measurementId: "G-6HYZHP2W3S"
-};
